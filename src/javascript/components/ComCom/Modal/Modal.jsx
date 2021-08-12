e1bc1f0c8e62ee6fa356c821d14a362794ff9876
Subash P <subash.p@mrcooper.com>
2021-08-13 00:07:27 +0530

    minor refactore

--- src/javascript/components/ComCom/Modal/Modal.jsx
+++ src/javascript/components/ComCom/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import ReactDOM from "react-dom";
 import Close from '../../../../images/close.svg'
 import PropTypes from "prop-types";
